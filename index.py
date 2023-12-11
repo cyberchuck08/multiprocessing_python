41d3e5d6f25eb18e322f81988589b344ceff2ba0
Reuben Nkukuu <eighfinity08@gmail.com>
2023-12-11 23:01:59 +0300

    second commit

--- index.py
+++ index.py
@@ -1,4 +1,25 @@
-for x in range(10):
-    print("Nothing")
-for _ in range(10):
-    print(_)
+import time
+import multiprocessing
+
+start = time.perf_counter()
+
+def do_something():
+    print("Sleeping 1 sec")
+    time.sleep(1)
+    print("Done Sleeping...")
+
+processes = []
+
+for p in range(10):
+    p = multiprocessing.Process(target=do_something)
+    p.start()
+    processes.append(p)
+
+for p in processes:
+    p.join()
+
+
+
+finish = time.perf_counter()
+
+print(f"Finished in {round(finish-start, 2)} second(s)")
